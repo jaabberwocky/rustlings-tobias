9ad5eeb69ae4edb76903a1371c9c6beff60525fa
tobias <tobias@tobias-leong.dev>
2023-08-29 23:16:41 +0800

    Add from_into.rs

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let mut split: Vec<&str> = s.split(',').collect();
+        if split.len() != 2 {
+            return Person::default();
+        }
+
+        let name = match split[0] {
+            "" => return Person::default(),
+            name => String::from(name),
+        };
+
+        let age = match split[1].parse::<usize>() {
+            Ok(age) => age,
+            Err(_) => return Person::default(),
+        };
+
+        Person {
+            name: String::from(name),
+            age,
+        }
     }
 }
 
