8e97a5c8e34b0db8b977525b3f2739694af162ed
tobias <tobias@tobias.dev>
2023-11-01 00:33:24 +0900

    Finish enums, strings and structs

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -5,8 +5,6 @@
 // Execute `rustlings hint structs2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -16,6 +14,32 @@ struct Order {
     made_by_email: bool,
     item_number: u32,
     count: u32,
+}
+
+impl Order {
+    fn new(
+        name: String,
+        year: u32,
+        made_by_phone: bool,
+        made_by_mobile: bool,
+        made_by_email: bool,
+        item_number: u32,
+        count: u32,
+    ) -> Self {
+        Order {
+            name,
+            year,
+            made_by_phone,
+            made_by_mobile,
+            made_by_email,
+            item_number,
+            count,
+        }
+    }
+
+    fn get_name(&self) -> &str {
+        &self.name
+    }
 }
 
 fn create_order_template() -> Order {
@@ -38,7 +62,17 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = Order::new(
+            String::from("Hacker in Rust"),
+            order_template.year,
+            order_template.made_by_phone,
+            order_template.made_by_mobile,
+            order_template.made_by_email,
+            order_template.item_number,
+            1,
+        );
+        println!("Your order: {}", your_order.get_name());
+
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
